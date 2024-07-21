801e26aaa93e5d8c18ce721da7203ae978479f70
Beatriz Trivillin <beatriztrivillin@nic.br>
2024-07-21 10:49:26 -0300

    alterando datasource

--- app/core/datasource.php
+++ app/core/datasource.php
@@ -1,45 +1,7 @@
 <?php
 
-// $datasource = array(
-//     'servername' => 'localhost',
-//     'username' => 'root',
-//     'password' => '1Sol1Lua@',
-//     'dbname' => 'to_do_list'
-// );
-
-// class DatasourceConnection
-// {
-//     public function __construct($datasource)
-//     {
-
-//         $conn = new mysqli($datasource['servername'], $datasource['username'], $datasource['password'], $datasource['dbname']);
-
-//         if ($conn->connect_error) {
-//             die("Conexão falhou: " . $conn->connect_error);
-//         }
-
-//         echo "Conectado com sucesso";
-
-//         $sql = "SELECT nome, descricao, status, data_finalizacao FROM tarefas";
-//         $result = $conn->query($sql);
-
-//         if (!empty($result->num_rows)) {
-//             while ($row = $result->fetch_assoc()) {
-//                 echo "id: " . $row["nome"] .$row["descricao"] .$row["status"].$row["data_finalizacao"] . "<br>";
-//             }
-//         } else {
-//             echo "0 results";
-//         }
-
-//         $conn->close();
-//     }
-// }
-
-// new DatasourceConnection($datasource);
-
 class DatasourceConnection
 {
-    // $config = Config::getConfig();
     protected $conn;
 
     public function __construct($config)
--- app/elements/time.php
+++ app/elements/time.php
@@ -1,3 +1,11 @@
 <div>
-    
+<?php
+// Obtém o timestamp atual
+$timestamp = time();
+
+// Formata a data e hora
+$data_hora = date('Y-m-d H:i:s', $timestamp);
+
+echo "Data e Hora atual: " . $data_hora;
+?>
 </div>
